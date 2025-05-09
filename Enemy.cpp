6fab26a74270f859672691aff9b676e103fef7cb
py5552 <pieter.w.pretorius@gmail.com>
2025-05-09 10:46:25 -0500

    a

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "Enemy.h"
+#include "Queue.h"
 
 enemy:enemy() {
 	height = 0;
 	width = 0;
+}
+
+enenmy:findPath(int targetHeight, int targetLength) {
+	Queue quew;
 }
