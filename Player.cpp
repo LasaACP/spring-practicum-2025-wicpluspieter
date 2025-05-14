14e5a225427fe3c587b9cdcbb4b37e6d27bd404c
py5552 <pieter.w.pretorius@gmail.com>
2025-05-14 10:34:22 -0500

    a

--- Player.cpp
+++ Player.cpp
@@ -3,13 +3,13 @@
 #include "Queue.h"
 #include <vector>
 using namespace std;
-player:player(int r, int c, Maze * m) {
+player::player(int r, int c, Maze * m) {
 	row = r;
 	column = c;
 	maze = m;
 }
 
-void player:updateLocation(int new_row, int new_column) {
+void player::updateLocation(int new_row, int new_column) {
 	row = new_row;
 	column = new_column;
 }
